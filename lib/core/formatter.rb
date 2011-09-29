291b1a7063c5adab4eef280ec5f0891e542bd1cc
Leszek Kalwa <leszek.kalwa@gmail.com>
2011-09-29 14:21:07 +0200

    Questionnarie config

--- lib/core/formatter.rb
+++ lib/core/formatter.rb
@@ -2,32 +2,52 @@ puts "loaded #{__FILE__}"
 
 module Questionnaire
   class Formatter
-  
-    def self.create_form_body(fields, f)
-      output = ''
-      fields.keys.each do |section|
-        next if fields[section].nil?
-        fields[section].each_pair do |k,v|
-           output << field_with_options(k, v, f)
-        end
-      end
-      output << f.button(:submit)
-      output.html_safe
-    end
-    
-    def self.field_with_options(k, v, f)
-      v.nil? ? simple_field(k,f) : with_options(k,v,f)
-    end
-    
-    # Simple fields without any additional options
-    def self.simple_field(k,f)
-      f.input k.to_sym
-    end
-    
-    # Field with additional options :only, :as etc..
-    def self.with_options(k,v,f)
-      simple_field(k,f) #temporary mock
-    end
 
+    OPTIONS = {default_element: :as}
+
+     class << self
+       extend ActiveSupport::Memoizable
+
+      def create_form_body(fields, f)
+        output = ''
+        fields.keys.each do |section|
+          next if fields[section].nil?
+          fields[section].each_pair do |k,v|
+             output << field_with_options(k, v, f)
+          end
+        end
+        output << f.button(:submit)
+        output.html_safe
+      end
+
+      def field_with_options(k, v, f)
+        v.nil? ? simple_field(k,f) : with_options(k,v,f)
+      end
+
+      # Simple fields without any additional options
+      def simple_field(k,f)
+        f.input(k.to_sym, attributes)
+      end
+
+      # grabs elements defined in gem initializer file, select getters, check if them are't nil,
+      # and maps them onto simple_form hashes (temporary solution)
+      def attributes
+        opt = {}
+        methods = Questionnaire.instance_methods.select { |m| m.to_s !~ /=/ }
+
+        methods.each do |m|
+          if result = Questionnaire.send(m.to_sym)
+            opt.merge!({OPTIONS[m] => result})
+          end
+        end
+        opt
+      end
+      memoize :attributes
+
+      # Field with additional options :only, :as etc..
+      def with_options(k,v,f)
+        simple_field(k,f) #temporary mock
+      end
+    end
   end
 end
--- lib/questionnaire.rb
+++ lib/questionnaire.rb
@@ -6,5 +6,16 @@ module Questionnaire
   #TODO: investigate why autoload Parser, require ... didnt work
   require   'core/parser'
   require   'core/form_helper'
-  require   'core/formatter' 
+  require   'core/formatter'
+
+
+  # define global options for fields as a module accessors here, consider using the same names as
+  # simple_form does
+  mattr_accessor :default_element
+
+  def self.setup
+    yield self
+  end
+
+
 end
