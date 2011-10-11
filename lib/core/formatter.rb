cdb73841cbc15a2ddff4ede968b976c79bdcde38
Leszek Kalwa <leszek.kalwa@gmail.com>
2011-10-11 15:10:55 +0200

    Refactored questionnaire

--- lib/core/formatter.rb
+++ lib/core/formatter.rb
@@ -1,7 +1,7 @@
 module Questionnaire
   class Formatter
 
-    OPTIONS = {default_element: :as}
+    NON_FIELD_OPTIONS = [:title_tag, :section_tag]
 
     class << self
       include ActionView::Helpers
@@ -13,43 +13,39 @@ module Questionnaire
           next if fields[section].nil?
           output << content_tag(Questionnaire.section_tag, I18n.t(:"questionnaires.#{key}.#{section}.label"))
           fields[section].each_pair do |section_field, field_options|
-            output << field_with_options(form, object, key, section_field, field_options, section)
+            output << field_with_options(form, object, key, section_field, section, field_options)
           end
         end
         output << form.button(:submit)
         output.html_safe
       end
 
-      def field_with_options(form, object, key,  field, options, section)
-        options.nil? ? simple_field(form, object, key, field, section) : with_options(form, object, key, field, options, section)
-      end
-
-      # Simple fields without any additional options                                             ,
-      def simple_field(form, object, key, field, section)
-        form.input field.to_sym, field_attributes.merge!(html_options(field, object, key, section))
-      end
-
-      # grabs elements defined in gem initializer file, select getters, check if them are't nil,
-      # and maps them onto simple_form hashes (temporary solution)
-      def field_attributes
-        #    methods = Questionnaire.instance_methods.select { |m| m.to_s !~ /=/ }
-        #    methods.inject({}) { |opt, met| OPTIONS[met] = result if (result = Questionnaire.send(met.to_sym)) }
-        #    opt
-        {}
-      end
-
-      memoize :field_attributes
-
-      def html_options(field, object, key, section)
-        {required: false}.tap do |h|
-          h[:label] = I18n.t(:"questionnaires.#{key}.#{section}.#{field}")
-          h[:input_html] = {value: object.send(key.to_sym).try(:[], field)}
+      def field_with_options(form, object, key, field, section, field_options)
+        if displayed? object, field_options
+          form.input field.to_sym, field_attributes.merge!(html_options(field, object, key, section, field_options))
         end
       end
 
-      # Field with additional options :only, :as etc..
-      def with_options(form, object, key, field, options, section)
-        simple_field(form, object, key, field, section) #temporary mock
+      def displayed?(object, field_options)
+        field_options.is_a?(Hash) && field_options.has_key?("only") ? object.send(:"#{field_options['only']}?") : true
+      end
+
+      # grabs elements defined in gem initializer file, select getters, check if them are't
+      # TODO convert to oneliner returning hash
+      def field_attributes
+        methods = Questionnaire.instance_methods.select { |m| m.to_s !~ /=/ } - NON_FIELD_OPTIONS
+        methods.inject({}) do |memo, method|
+          memo[method] = Questionnaire.send(method.to_sym)
+          memo
+        end
+      end
+
+      def html_options(field, object, key, section, options)
+        {required: false}.tap do |h|
+          h[:as] = options["as"].to_sym if options.is_a?(Hash) && options.has_key?("as")
+          h[:label] = I18n.t(:"questionnaires.#{key}.#{section}.#{field}")
+          h[:input_html] = {value: object.send(key.to_sym).try(:[], field)}
+        end
       end
     end
   end
